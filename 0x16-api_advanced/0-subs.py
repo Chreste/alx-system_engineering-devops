e63568727957f5ef7dec6e5ea95431dd8a16386e
CHRISTENA EHAB <kokoehab1999@gmail.com>
2024-05-07 02:31:41 -0700

    fi

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""0-subs module"""
+"""Module that consumes the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API
+    Args:
+        subreddit (str): subreddit
 
     Returns:
-        the number of subscribers for a given subreddit,
+        int: number of subscribers
     """
-    URL = 'https://www.reddit.com/r/'
-    res = requests.get('{}{}/about.json'.
-                       format(URL, subreddit),
-                       headers={'User-Agent': 'ALX-User-Agent'},
-                       allow_redirects=False)
-    if res.status_code != 200:
-        return 0
-    return res.json().get('data').get('subscribers')
+    base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
+    return 0
